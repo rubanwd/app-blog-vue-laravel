125173405c11f0f9f07da000ae8a595d43295dfc
Serhio Ruban <rubanwd@gmail.com>
2018-09-06 15:14:32 +0300

    second commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,67 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+window.VueRouter = require('vue-router').default;
 
-const app = new Vue({
-    el: '#app'
-});
+window.VueAxios = require('vue-axios').default;
+
+window.Axios = require('axios').default;
+
+let AppLayout = require('./components/App.vue');
+
+const Listposts = Vue.component('Listposts', require('./components/Listposts.vue'));
+
+const Addpost = Vue.component('Addpost', require('./components/Addpost.vue'));
+
+const Deletepost = Vue.component('Deletepost', require('./components/Deletepost.vue'));
+
+const Editpost = Vue.component('Editpost', require('./components/Editpost.vue'));
+
+const Viewpost = Vue.component('Viewpost', require('./components/Viewpost.vue'));
+
+
+Vue.use(VueRouter, VueAxios, Axios);
+
+
+const routes = [
+
+	{
+		name: 'Listposts',
+		path: '/',
+		component: Listposts
+	},
+	{
+		name: 'Addpost',
+		path: '/add-post',
+		component: Addpost
+	},
+	{
+		name: 'Editpost',
+		path: '/edit/:id',
+		component: Editpost
+	},
+	{
+		name: 'Deletepost',
+		path: '/delete-post',
+		component: Deletepost
+	},
+	{
+		name: 'Viewpost',
+		path: '/view/:id',
+		component: Viewpost
+	}
+
+];
+
+
+
+const router = new VueRouter({ mode: 'history', routes: routes});
+
+new Vue(
+	Vue.util.extend(
+		{router},
+		AppLayout
+		)
+	).$mount('#app');
---
+++ resources/js/components/Listposts.vue
@@ -0,0 +1,68 @@
+<template id="post-list">
+
+	<div class="row">
+		<div class="pull-right">
+			<router-link class="btn btn-xs btn-primary" v-bind:to="{path: '/add-post'}">
+				<span class="glyphicon glyphicon-plus">Add New Post</span>
+			</router-link>
+			<br><br>
+		</div>
+		<table class="table table-bordered">
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Post Title</th>
+					<th>Post Body</th>
+					<th class="col-md-3">Action</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="(post, index) in filteredPosts" >
+					<td>{{ index + 1 }}</td>
+					<td>{{ post.title }}</td>
+					<td>{{ post.body }}</td>
+					<td>
+						<router-link class="btn btn-xs btn-info" v-bind:to="{name: 'Viewpost', params: {id: post.id}}">Show</router-link>
+						<router-link class="btn btn-xs btn-warning" v-bind:to="{name: 'Editpost', params: {id: post.id}}">Edit</router-link>
+						<router-link class="btn btn-xs btn-danger" v-bind:to="{name: 'Deletepost', params: {id: post.id}}">Delete</router-link>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	
+</template>
+
+<script>
+	
+	export default {
+
+        data:function(){
+        	return {
+        		posts: ''
+        	}
+        },
+
+        created: function() {
+        	let url = 'http://localhost:8000/posts/';
+
+        	Axios.get(url).then((response) => {
+        		this.posts = response.data;
+        	});
+        },
+
+        computed: {
+        	filteredPosts: function(){
+        		if(this.posts.length) {
+        			return this.posts;
+        		}
+        	}
+        }
+
+    }
+
+</script>
+
+<style lang="css">
+	
+</style>
